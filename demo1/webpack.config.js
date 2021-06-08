06200436066a15ac1a0374b766824f1105954992
Campbell He <hkp18@mails.tsinghua.edu.cn>
2021-06-08 10:49:27 +0800

    refactor: Adjust code structure
    Install webpack in node_modules
    Install demo-specific dep in demoX/node_modules

--- demo1/webpack.config.js
+++ demo1/webpack.config.js
@@ -6,6 +6,11 @@ const configs = {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
   },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 9000,
+  },
 };
 
 module.exports = configs;
